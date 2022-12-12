2a1eeb97d39d796a450ddccd15762c7916dcce01
Даниил Манян <69975772+atsumuS@users.noreply.github.com>
2022-12-13 00:04:07 +0300

    Update users.py
    updated

--- app/users.py
+++ app/users.py
@@ -1,21 +1,27 @@
-## not ended file
-
-from fastapi import FastAPI, status, APIRouter
-from app.database import  *
-from app.schemas import *
-from typing import List
-
-router = APIRouter()
+from fastapi import APIRouter
+from .database import  *
+from .schemas import *
+from passlib.hash import pbkdf2_sha256
 
 
-@router.post("/users/", response_model=userschema) # posting user on server 
+
+
+
+
+router = APIRouter(
+    tags = ["User"], # changing our list on docs
+)
+
+
+@router.post("/users/", response_model=userschema) # create user on server 
 async def user(user: userschemain):
+    hashed_password = pbkdf2_sha256.hash(user.password)
+    
     query = User.insert().values(
     username = user.username, 
-    password = user.password
+    password = hashed_password
     )
+    
     last_record_id = await database.execute(query)
-    return {**user.dict(), "id": last_record_id}
-
-
-#TODO не законченный файл, так как нет роутера логинки :(
+    return {**user.dict(), "id": last_record_id, 
+            'message': 'User Created!'}
