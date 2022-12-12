ac9dc2cd21524ecc2ebb8664984dc501c1bd1f41
Даниил Манян <69975772+atsumuS@users.noreply.github.com>
2022-12-13 00:02:57 +0300

    Update main.py
    updated

--- app/main.py
+++ app/main.py
@@ -1,12 +1,18 @@
-from fastapi import FastAPI, status
-from .database import *
+from typing import Union
+
+
+from fastapi import FastAPI
+from app import auth
 from .database import *
 from . import bookses, users
 import uvicorn
 
 
 
-app = FastAPI()
+
+app = FastAPI(
+    tags = ["Books"], # changing our list on docs 
+)
 
 
 
@@ -22,6 +28,7 @@ async def startup():
 
 app.include_router(bookses.router)
 app.include_router(users.router)
+app.include_router(auth.router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host = "127.0.0.1", port = 8000)
